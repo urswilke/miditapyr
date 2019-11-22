43fb065774413116e1e0b77fd89e207b74366af1
Urs Wilke <urs.wilke@gmail.com>
2019-11-22 17:29:00 +0100

    rewrite mid file writing function

--- python/py/py_funs.py
+++ python/py/py_funs.py
@@ -182,3 +182,44 @@ def df2mido_midifile(df, ticks_per_beat):
                                  velocity=df.loc[i, 'velocity'].astype(int), 
                                  time=df.loc[i, 'time'].astype(int)))
     return outfile
+
+def df2mido_midifile(df_meta, df_notes, ticks_per_beat):
+    from mido import MetaMessage, Message, MidiFile, MidiTrack
+    df_notes = df_notes.reset_index()
+    outfile = MidiFile(ticks_per_beat=ticks_per_beat)
+
+    track = MidiTrack()
+    
+    track.append(MetaMessage('set_tempo', tempo = int(df_meta.loc[df_meta.type == 'set_tempo', 'tempo'])))
+    track.append(MetaMessage('time_signature',
+                             numerator = int(df_meta.loc[df_meta.type == 'time_signature', 'numerator']), 
+                             denominator = int(df_meta.loc[df_meta.type == 'time_signature', 'denominator'])))#, 
+    #                              clocks_per_click = df.loc[i, 'clocks_per_click'], 
+    #                              notated_32nd_notes_per_beat = df.loc(i, 'notated_32nd_notes_per_beat')))
+    
+    # track.append(Message('program_change', program=12))
+    new_track_row = np.array(df_notes.i_track != df_notes.i_track.shift(1))
+    last_in_track_row = np.array(df_notes.i_track != df_notes.i_track.shift(1))
+    end_of_track_times = np.array(df_meta.loc[df_meta.type == 'end_of_track', 'time'])
+    i_track = 0
+    for i in range(len(df_notes)):
+        # meta messages:
+        # if df.loc[i, 'type'] == 'track_name':
+        if new_track_row[i]:
+            track = MidiTrack()
+            outfile.tracks.append(track)
+        if last_in_track_row[i]:
+            track.append(MetaMessage('end_of_track', time = end_of_track_times[i_track]))
+            i_track += 1
+        if df_notes.loc[i, 'type'] == 'note_on':
+            track.append(Message('note_on', 
+                                 note=df_notes.loc[i, 'note'].astype(int), 
+                                 velocity=df_notes.loc[i, 'velocity'].astype(int), 
+                                 channel=df_notes.loc[i, 'channel'].astype(int), 
+                                 time=df_notes.loc[i, 'time'].astype(int)))
+        elif df_notes.loc[i, 'type'] == 'note_off':
+            track.append(Message('note_off', 
+                                 note=df_notes.loc[i, 'note'].astype(int), 
+                                 velocity=df_notes.loc[i, 'velocity'].astype(int), 
+                                 time=df_notes.loc[i, 'time'].astype(int)))
+    return outfile
