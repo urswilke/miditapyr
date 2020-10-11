071aed1d7868324602717922fdcf3445843142bf
Urs Wile <urs.wilke@gmail.com>
2020-10-11 15:01:15 +0200

    add new functions

--- miditapyr/__init__.py
+++ miditapyr/__init__.py
@@ -1,5 +1,4 @@
-from pandas import DataFrame 
+from pandas import DataFrame
 from mido import MidiTrack, MidiFile, MetaMessage, Message
 import numpy as np
-from .mido_io import df2mido_midifile, mido_midi_df, df_2_midi 
-
+from .mido_io import df2mido_midifile, mido_midi_df, df_2_midi, tidy_df, midi_to_df, split_df, compact_df, df_to_midi 
