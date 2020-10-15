54739beaff2fb0b8f355a73b5c73ae169ac93f13
Urs Wile <urs.wilke@gmail.com>
2020-10-15 14:25:20 +0200

    update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name='miditapyr',
-    version='0.0.0.9000',
+    version='0.0.1',
     description='Tabulate midi data in DataFrames and write back to midi files',
     long_description=README,
     long_description_content_type="text/markdown",
