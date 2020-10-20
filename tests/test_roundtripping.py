6afa1e623b4eef75dacf11c848739b575b964cd8
Urs Wilke <urs.wilke@gmail.com>
2020-10-20 23:54:40 +0200

    add roundtripping test

---
+++ tests/test_roundtripping.py
@@ -0,0 +1,8 @@
+import mido
+import miditapyr as mt
+
+def test_roundtripping():
+    mido_mid_file = mido.MidiFile(mt.get_test_midi_file())
+    dfc = mt.midi_to_df(mido_mid_file)
+
+    assert dfc.eq(mt.compact_df(mt.tidy_df(dfc)))
