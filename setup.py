6bbde0b6766283dc90c01e1f51747db07da69b82
Urs Wilke <urs.wilke@gmail.com>
2020-10-21 01:02:01 +0200

    change setup paths
    https://stackoverflow.com/questions/51286928/what-is-where-argument-for-in-setuptools-find-packages
    https://stackoverflow.com/questions/17155804/confused-about-the-package-dir-and-packages-settings-in-setup-py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-import setuptools
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -7,7 +7,7 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE / "README.md").read_text()
 
-setuptools.setup(
+setup(
     name='miditapyr',
     version='0.0.1',
     description='Tabulate midi data in DataFrames and write back to midi files',
@@ -17,9 +17,9 @@ setuptools.setup(
     author='Urs Wilke',
     author_email='urs.wilke@gmail.com',
     license='MIT',
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'pandas',
-        'numpy',
-        'mido'],
+    packages=find_packages(),
+    install_requires=find_packages(where='.'),
+    package_dir={
+        '': '.',
+    },
     zip_safe=False)
