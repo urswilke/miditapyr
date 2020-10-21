ef6cc273c29f9407bad0beb91cbdc5ff4d29fad8
Urs Wilke <urs.wilke@gmail.com>
2020-10-21 12:40:03 +0200

    use new get_test_midi_file() function

--- tests/test_roundtripping.py
+++ tests/test_roundtripping.py
@@ -2,7 +2,8 @@ import mido
 import miditapyr as mt
 
 def test_roundtripping():
-    mido_mid_file = mido.MidiFile(mt.get_test_midi_file())
+    # mido_mid_file = mido.MidiFile(mt.get_test_midi_file())
+    mido_mid_file = mt.get_test_midi_file()
     dfc = mt.midi_to_df(mido_mid_file)
 
     assert dfc.eq(mt.compact_df(mt.tidy_df(dfc))).all(axis=1).all()
