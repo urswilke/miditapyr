cc751fe9c3d008bf3486bcb4426c1747911dd0e1
Urs Wilke <urs.wilke@gmail.com>
2020-10-21 12:39:44 +0200

    check if all elements in dataframes are equal

--- tests/test_roundtripping.py
+++ tests/test_roundtripping.py
@@ -5,4 +5,4 @@ def test_roundtripping():
     mido_mid_file = mido.MidiFile(mt.get_test_midi_file())
     dfc = mt.midi_to_df(mido_mid_file)
 
-    assert dfc.eq(mt.compact_df(mt.tidy_df(dfc)))
+    assert dfc.eq(mt.compact_df(mt.tidy_df(dfc))).all(axis=1).all()
