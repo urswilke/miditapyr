24cca4e55a74bdab6ecb4690dd73a85173dd530e
Urs Wilke <urs.wilke@gmail.com>
2021-11-04 23:11:33 +0100

    try to fix rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../miditapyr/'))
-
+sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.'))), 'miditapyr'))
 
 # -- Project information -----------------------------------------------------
 
