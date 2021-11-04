17a79fef44d6f95a67bafed03ff28b3e1663f313
Urs Wilke <urs.wilke@gmail.com>
2021-11-04 23:26:36 +0100

    last try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('.'))), 'miditapyr'))
+sys.path.insert(0, os.path.join(os.path.dirname((os.path.abspath('../..'))), 'miditapyr'))
 
 # -- Project information -----------------------------------------------------
 
