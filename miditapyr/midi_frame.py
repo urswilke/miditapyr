4c4141daacbc5a38439154f1a787f6f73ffafcaf
Urs Wilke <urs.wilke@gmail.com>
2021-11-15 20:56:43 +0100

    rename functions:         * `midi_to_df` to `frame_midi`         * `tidy_df` to `unnest_midi`         * `compact_df` to `nest_midi`         * `df_to_midi` to `write_midi`

--- miditapyr/midi_frame.py
+++ miditapyr/midi_frame.py
@@ -10,11 +10,11 @@ class MidiFrameTidy:
     :obj:`midi_frame_tidy` can be updated with :meth:`update_tidy_mf`. 
     This also triggers an update of objects of the observing class :class:`MidiFrameCompact`.
 
-    :param midi_frame_raw: :class:`~pandas.DataFrame` resulting of :func:`~mido_io.midi_to_df`.
+    :param midi_frame_raw: :class:`~pandas.DataFrame` resulting of :func:`~mido_io.frame_midi`.
     """
     def __init__(self, midi_frame_raw):
         self._observers = []
-        self.midi_frame_tidy = mido_io.tidy_df(midi_frame_raw)
+        self.midi_frame_tidy = mido_io.unnest_midi(midi_frame_raw)
 
     def register_observer(self, observer):
         self._observers.append(observer)
@@ -31,17 +31,17 @@ class MidiFrameCompact:
     When the observed object :obj:`midi_frame_tidy` is updated with :meth:`MidiFrameTidy.update_tidy_mf`, :obj:`midi_frame_compact`
     is also automatically updated with :meth:`MidiFrameCompact.update_mf_compact`.
 
-    :param midi_frame_tidy: :class:`~pandas.DataFrame` resulting of :func:`~miditapyr.mido_io.tidy_df`.
+    :param midi_frame_tidy: :class:`~pandas.DataFrame` resulting of :func:`~miditapyr.mido_io.unnest_midi`.
     """
     def __init__(self, midi_frame_tidy):
         midi_frame_tidy.register_observer(self)
-        self.midi_frame_compact = mido_io.compact_df(
+        self.midi_frame_compact = mido_io.nest_midi(
             midi_frame_tidy.midi_frame_tidy, 
             repair_reticulate_conversion = True
         )
 
     def update_mf_compact(self, midi_frame_tidy, tidy_mf_mod):
-        self.midi_frame_compact = mido_io.compact_df(
+        self.midi_frame_compact = mido_io.nest_midi(
             tidy_mf_mod, 
             repair_reticulate_conversion = True
         )
@@ -51,9 +51,9 @@ class MidiFrames(object):
     Structure that reads in a midi file and has the following attributes:
     
     * :attr:`midi_file`: The midi data as a :class:`~mido.MidiFile` object.
-    * :attr:`midi_frame_raw`:  :class:`~pandas.DataFrame` returned by :func:`~miditapyr.mido_io.midi_to_df`.
-    * :attr:`midi_frame_tidy`:  :class:`~miditapyr.midi_frame.MidiFrameTidy` object (contains :obj:`midi_frame_tidy.midi_frame_tidy`, a :class:`~pandas.DataFrame` returned by :func:`~miditapyr.mido_io.tidy_df`).
-    * :attr:`midi_frame_compact`: :class:`~miditapyr.midi_frame.MidiFrameCompact` object (contains :obj:`midi_frame_compact.midi_frame_compact`, a :class:`~pandas.DataFrame` returned by :func:`~miditapyr.mido_io.compact_df`).
+    * :attr:`midi_frame_raw`:  :class:`~pandas.DataFrame` returned by :func:`~miditapyr.mido_io.frame_midi`.
+    * :attr:`midi_frame_tidy`:  :class:`~miditapyr.midi_frame.MidiFrameTidy` object (contains :obj:`midi_frame_tidy.midi_frame_tidy`, a :class:`~pandas.DataFrame` returned by :func:`~miditapyr.mido_io.unnest_midi`).
+    * :attr:`midi_frame_compact`: :class:`~miditapyr.midi_frame.MidiFrameCompact` object (contains :obj:`midi_frame_compact.midi_frame_compact`, a :class:`~pandas.DataFrame` returned by :func:`~miditapyr.mido_io.nest_midi`).
     * :meth:`~MidiFrames.write_file`: Writes back the midi data to a midi file.
     
     The dataframe :attr:`midi_frame_tidy.midi_frame_tidy` can be manipulated with the method :meth:`~miditapyr.midi_frame.MidiFrameTidy.update_tidy_mf`.
@@ -70,7 +70,7 @@ class MidiFrames(object):
     """
     def __init__(self, midi_file_string):
         self.midi_file = MidiFile(midi_file_string)
-        self.midi_frame_raw = mido_io.midi_to_df(self.midi_file)
+        self.midi_frame_raw = mido_io.frame_midi(self.midi_file)
         self.midi_frame_tidy = MidiFrameTidy(self.midi_frame_raw)
         self.midi_frame_compact = MidiFrameCompact(self.midi_frame_tidy) 
 
@@ -79,7 +79,7 @@ class MidiFrames(object):
 
         :param out_file_string: midi file path where the file should be stored.
         """
-        mido_io.df_to_midi(
+        mido_io.write_midi(
             self.midi_frame_compact.midi_frame_compact,
             self.midi_file.ticks_per_beat,
             out_file_string
--- tests/test_roundtripping.py
+++ tests/test_roundtripping.py
@@ -4,10 +4,10 @@ import miditapyr.mido_io as mt
 
 
 mido_midi_file = mt.get_test_midi_file()
-dfc0 = mt.midi_to_df(mido_midi_file)
-dft0 = mt.tidy_df(dfc0)
-dfc1 = mt.compact_df(dft0, True)
-dft1 = mt.tidy_df(dfc1)
+dfc0 = mt.frame_midi(mido_midi_file)
+dft0 = mt.unnest_midi(dfc0)
+dfc1 = mt.nest_midi(dft0, True)
+dft1 = mt.unnest_midi(dfc1)
 
 def test_tidy_compact_roundtripping():
     # test that from tidy -> compact -> back to tidy results in the same
@@ -24,26 +24,26 @@ def test_save_read_roundtripping(tmp_path):
     filename2 = 'temp_midifile' + '2' + '.mid'
     d.mkdir()
     path2 = d / filename2
-    mt.df_to_midi(dfc1, mido_midi_file.ticks_per_beat, path2)
+    mt.write_midi(dfc1, mido_midi_file.ticks_per_beat, path2)
     mido_midi_file2 = mido.MidiFile(path2)
-    dfc2 = mt.midi_to_df(mido_midi_file2)
-    dft2 = mt.tidy_df(dfc2)
+    dfc2 = mt.frame_midi(mido_midi_file2)
+    dft2 = mt.unnest_midi(dfc2)
 
     d = tmp_path / "sub"
     filename3 = 'temp_midifile' + '3' + '.mid'
     path3 = d / filename3
-    mt.df_to_midi(dfc2, mido_midi_file.ticks_per_beat, path3)
+    mt.write_midi(dfc2, mido_midi_file.ticks_per_beat, path3)
     mido_midi_file3 = mido.MidiFile(path3)
-    dfc3 = mt.midi_to_df(mido_midi_file3)
-    dft3 = mt.tidy_df(dfc3)
+    dfc3 = mt.frame_midi(mido_midi_file3)
+    dft3 = mt.unnest_midi(dfc3)
 
     d = tmp_path / "sub"
     filename4 = 'temp_midifile' + '4' + '.mid'
     path4 = d / filename4
-    mt.df_to_midi(dfc3, mido_midi_file.ticks_per_beat, path4)
+    mt.write_midi(dfc3, mido_midi_file.ticks_per_beat, path4)
     mido_midi_file4 = mido.MidiFile(path4)
-    dfc4 = mt.midi_to_df(mido_midi_file4)
-    dft4 = mt.tidy_df(dfc4)
+    dfc4 = mt.frame_midi(mido_midi_file4)
+    dft4 = mt.unnest_midi(dfc4)
 
     # The tests only pass for midifiles that have been generated from dataframes
     # in miditapyr:
