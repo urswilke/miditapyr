255d9137c8c0a2bc41cd25b5fd8bb24d2e027355
Urs Wilke <urs.wilke@gmail.com>
2021-11-17 23:29:39 +0100

    bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Urs Wilke'
 author = 'Urs Wilke'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
